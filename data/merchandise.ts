05222feff6d641f077f186abeb0fbb4d4312730b
brooksvillecasting-cyber <brooksvillecasting@gmail.com>
2025-12-08 22:06:55 -0500

    Add files via upload

---
+++ data/merchandise.ts
@@ -0,0 +1,45 @@
+
+import { MerchItem } from '@/types';
+
+export const merchandise: MerchItem[] = [
+  {
+    id: 'tshirt-black',
+    name: 'Afroman Official T-Shirt - Black',
+    description: 'Premium quality cotton t-shirt with official Afroman logo',
+    price: 39.99,
+    imageUrl: require('@/assets/images/9957216d-aaad-4d60-b360-5fbe38a62452.png'),
+    sizes: ['S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL'],
+    type: 'tshirt',
+    color: 'Black',
+  },
+  {
+    id: 'tshirt-white',
+    name: 'Afroman Official T-Shirt - White',
+    description: 'Premium quality cotton t-shirt with official Afroman logo',
+    price: 39.99,
+    imageUrl: require('@/assets/images/a61bc566-5ba4-4b73-ae79-f011a3d768f1.png'),
+    sizes: ['S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL'],
+    type: 'tshirt',
+    color: 'White',
+  },
+  {
+    id: 'hoodie-black',
+    name: 'Afroman Official Hoodie - Black',
+    description: 'Comfortable and stylish hoodie with official Afroman branding',
+    price: 49.99,
+    imageUrl: require('@/assets/images/0b1b0006-dfc8-4ff3-b7d5-72058a084143.png'),
+    sizes: ['S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL'],
+    type: 'hoodie',
+    color: 'Black',
+  },
+  {
+    id: 'hoodie-white',
+    name: 'Afroman Official Hoodie - White',
+    description: 'Comfortable and stylish hoodie with official Afroman branding',
+    price: 49.99,
+    imageUrl: require('@/assets/images/b3ad6fba-39e9-460f-8e13-34529f70d0af.png'),
+    sizes: ['S', 'M', 'L', 'XL', '2XL', '3XL', '4XL', '5XL'],
+    type: 'hoodie',
+    color: 'White',
+  },
+];
